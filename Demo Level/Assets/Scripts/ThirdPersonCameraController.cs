45ef0c7eb424f916e4f945aa3f51648d4da473fb
marcmulders <marc_mulders@live.nl>
2020-05-05 14:59:55 +0200

    Game - Demo level

---
+++ Demo Level/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    
+    public float rotationSpeed = 1.0f;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    private void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35.0f, 60.0f);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0.0f);
+        Player.rotation = Quaternion.Euler(0.0f, mouseX, 0.0f);
+    }
+}
---
+++ Demo Level/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float moveSpeedDefault;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        float moveSpeed = moveSpeedDefault;
+        if(Input.GetKey(KeyCode.LeftShift)){
+            moveSpeed = moveSpeedDefault*2;
+        }
+        else {
+            moveSpeed = moveSpeedDefault;
+        }
+
+        Vector3 playerMovement = new Vector3(hor,0f,ver) * moveSpeed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
